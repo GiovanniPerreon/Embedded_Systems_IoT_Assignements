08d2f35828108dd7e87732213792f415e77eda4b
Daniel Alejandro Horna <d.alejandrohorna@gmail.com>
2026-01-20 21:35:55 +0100

    Fix Java 21 compatibility and module exports
    - Update pom.xml to target Java 21
    - Add transitive exports for JavaFX modules in module-info.java

--- Assignment_2/drone-remote-unit/src/main/java/module-info.java
+++ Assignment_2/drone-remote-unit/src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module dru {
-    requires javafx.controls;
-    requires javafx.fxml;
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+    requires transitive javafx.graphics;
+    requires transitive javafx.base;
     requires com.fazecast.jSerialComm;
 
     opens dru to javafx.fxml;
     opens dru.ui to javafx.fxml;
+    opens dru.model to javafx.base;
 
     exports dru;
     exports dru.ui;
