8efcd3ab8921f47f6d86fea6d6b728e8031d949b
Filippo Patrignani <filippo.patrignani64@gmail.com>
2026-02-03 15:02:38 +0100

    dbs added

---
+++ Assignment_3/dbs/README.md
@@ -0,0 +1,4 @@
+Istruzioni per avviare il server statico per la dashboard con python su windows:
+Aprire il terminale nel folder del progetto poi:
+cd dbs/public
+python -m http.server 5500
---
+++ Assignment_3/dbs/public/config.js
@@ -0,0 +1,7 @@
+// Dashboard config file
+// Questo valore DEVE corrispondere a Config.HTTP_PORT in Java
+window.CONFIG_HTTP_PORT = 5500;
+// Base URL per le API del backend
+window.CONFIG_API_BASE = `http://localhost:${window.CONFIG_HTTP_PORT}/api`;
+// Intervallo di polling in ms
+window.CONFIG_DOM_UPDATE_RATE = 100;
